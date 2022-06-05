4ccea1f523ff2963c9f8624c1349960452933b7a
Viha Gupta <vihagupta95@gmail.com>
2022-06-05 15:26:19 -0400

    added typewriter sounds + newline support

--- src/components/EditPage.tsx
+++ src/components/EditPage.tsx
@@ -3,6 +3,21 @@ import { Box, Button, Heading, Textarea } from '@chakra-ui/react';
 import { FiEdit2 } from 'react-icons/fi';
 import Typewriter from 'typewriter-effect';
 
+
+
+function playAudio(id) { 
+    var x = document.getElementById(id); 
+    x.play(); 
+} 
+function pauseAudio(id) {
+    var x = document.getElementById(id); 
+    x.pause();
+}
+function loadAudio(id) {
+    var x = document.getElementById(id); 
+    x.load();
+}
+
 export const EditPage = () => {
     const [postContent, setPostContent] = useState('');
 
@@ -17,6 +32,7 @@ export const EditPage = () => {
                         pauseFor: 900000,
                     }}
                 />
+
             </Heading>
             <Textarea
                 id="edit-container"
@@ -27,7 +43,37 @@ export const EditPage = () => {
                 minHeight="400px"
                 maxHeight="400px"
                 placeholder="Write your heart out..."
-                onChange={(event) => setPostContent(event.target.value)}
+                onKeyDown={(event) => {
+                    console.log(event.which)
+                    const key = event.which
+                    // space
+                    if (key == 32){
+                        pauseAudio("space-bar-audio")
+                        loadAudio("space-bar-audio")
+                        playAudio("space-bar-audio")
+                    }
+                    // return
+                    else if (key == 13){
+                        pauseAudio("return-audio")
+                        loadAudio("return-audio")
+                        playAudio("return-audio")
+                    } 
+                    // backspace
+                    else if (key == 8){
+                        pauseAudio("backspace-audio")
+                        loadAudio("backspace-audio")
+                        playAudio("backspace-audio")
+                    }
+                    // default to all keys
+                    else {
+                        pauseAudio("key-audio")
+                        loadAudio("key-audio")
+                        playAudio("key-audio")
+                    }
+                }}
+                onChange={(event) => {
+                    setPostContent(event.target.value)
+                }}
                 autoFocus
             />
             <Button leftIcon={<FiEdit2 />} id="publish-btn" onClick={() => {
--- src/components/ViewPage.tsx
+++ src/components/ViewPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Text, Heading } from '@chakra-ui/react';
+import { Box, Text, Heading, Link } from '@chakra-ui/react';
 import Typewriter from 'typewriter-effect';
 
 type ViewPageProps = {
@@ -10,17 +10,20 @@ export const ViewPage = (props: ViewPageProps) => {
     const { post } = props;
     return (
         <Box p='12' w='100%' h='100vh' display='flex' flexDirection='column' alignItems='center'>
-            <Heading as={'h4'} size="sm" fontFamily='monospace'>
-                <Typewriter
-                    options={{
-                        strings: ['Mini Blog'],
-                        autoStart: true,
-                        loop: false,
-                        pauseFor: 900000,
-                    }}
-                />
-            </Heading>
+            <Link href='/' _hover={{ textDecoration:"none" }} _focus={{ boxShadow:"none" }}>
+                <Heading as={'h4'} size="sm" fontFamily='monospace'>
+                    <Typewriter
+                        options={{
+                            strings: ['Mini Blog'],
+                            autoStart: true,
+                            loop: false,
+                            pauseFor: 900000,
+                        }}
+                    />
+                </Heading>
+            </Link>
             <Text
+                whiteSpace="pre-line"
                 id="view-container"
                 size="lg"
                 m="50px"
