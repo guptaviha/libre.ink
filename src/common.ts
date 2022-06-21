2176db05b98159811df74f1f09c849b567649837
Karan Rajpal <karanrajpal001@gmail.com>
2022-06-21 19:14:13 -0400

    importing app title

--- src/common.ts
+++ src/common.ts
@@ -1,3 +1,5 @@
+import { APP_TITLE } from "./constants";
+
 export const setTitle = (postContent: string) => {
     const plainTextFirstLine = postContent.split('\n')[0].replace(/[#*>`]/g, '').substring(0, 150);
     const title = plainTextFirstLine.trim().length ? `${plainTextFirstLine} - ${APP_TITLE}` : APP_TITLE;
