689def1462cd716203651726b715a6c8e27f8eb6
Karan Rajpal <karanrajpal001@gmail.com>
2022-06-30 17:52:06 -0400

    hide cursor

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Typewriter from 'typewriter-effect';
-import { Box, Heading, Image, Link } from '@chakra-ui/react';
-import { APP_TITLE } from '../constants';
+import { Box, Image, Link } from '@chakra-ui/react';
 import ghostIconSW from '../../assets/icons/noun-ghost-sneaky-sw.svg';
 import ghostIconC from '../../assets/icons/noun-ghost-sneaky-c.svg';
 import ghostIconSE from '../../assets/icons/noun-ghost-sneaky-se.svg';
@@ -41,16 +39,6 @@ export const Logo = () => {
         <Link href='/' _hover={{ textDecoration: "none" }} _focus={{ boxShadow: "none" }}>
             <Box position='fixed' top='18px' left='10px' display='flex' alignItems='center'>
                 <Image src={icon} width='40px' paddingRight='5px' />
-                {/* <Heading size={{ base: 'lg', sm: 'md', md: 'md', lg: 'md' }} fontFamily='monospace' title='Your favorite anonymous publishing platform'>
-                    <Typewriter
-                        options={{
-                            strings: [APP_TITLE],
-                            autoStart: true,
-                            loop: false,
-                            pauseFor: 90000000,
-                        }}
-                    />
-                </Heading> */}
             </Box>
         </Link>
     )
