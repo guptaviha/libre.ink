8c2d3943e29e29332ddfe7010d02826f5e0cb3a3
Viha Gupta <vihagupta95@gmail.com>
2022-07-03 15:13:46 -0400

    moved text to constants file

--- src/constants.ts
+++ src/constants.ts
@@ -16,6 +16,49 @@ export const PLACEHOLDER_TEXT = `Instant anonymous blogging.
 
 Write. Publish. Take it and go.`;
 
+export const BUY_ME_A_BOBA_LINK = `https://www.buymeacoffee.com/vihagupta99`
+export const GITHUB_LINK = `https://github.com/guptaviha/libre.ink`
+export const FB_SHARE_LINK = `https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Flibre.ink%2F&amp;src=sdkpreparse`
+export const TWITTER_SHARE_LINK = `https://twitter.com/intent/tweet?text=Check%20this%20out!%20Minimalistic%20and%20quick%20blogging%20is%20here:%20libre.ink`
+export const TINY_URL_LINK = `https://tinyurl.com/`
+
+export const APP_TITLE_TOOLTIP = `Your favorite anonymous publishing platform`
+// export const MUTE_BTN_ON_TOOLTIP = `Sound Off`
+// export const MUTE_BTN_OFF_TOOLTIP = `Sound On`
+export const SHARE_BTN_TOOLTIP = `Share`
+export const FONT_BTN_TOOLTIP = `Font`
+// export const DARK_MODE_BTN_ON_TOOLTIP = `Lights Off`
+// export const DARK_MODE_BTN_OFF_TOOLTIP = `Lights On`
+export const INFO_BTN_TOOLTIP = `Info`
+export const CLIPBOARD_TOOLTIP = `Copy to Clipboard`
+export const TINY_URL_TOOLTIP = `URL Shortening with TinyURL`
+export const EMAIL_TOOLTIP = `Open Mail Client`
+
+export const CLIPBOARD_TOAST_TEXT = `URL copied to clipboard.`
+
+export const BOBA_HEADER_TEXT = `Help keep the lights on`
+export const BOBA_BODY_TEXT = `If you like what you see, consider supporting us.\n`
+export const BOBA_BTN_TEXT = `Buy us a Boba`
+export const GITHUB_STAR_BTN_TEXT = `Star us on GitHub`
+
+export const INFO_MODAL_HEADER_TEXT = `Welcome to libre.ink!`
+export const INFO_MODAL_BODY_TEXT = `The serverless instant-publish blog platform that lets you publish as fast as you can write.\n\nRemember to save your blog URL because all the content of your blog post lives in the URL. The application does not talk to a database or server of any kind.`
+
+export const TAG1_TEXT = `open-source`
+export const TAG2_TEXT = `serverless`
+export const TAG3_TEXT = `markdown-supported`
+
+export const FONT_HEADER_TEXT = `Font Settings`
+export const FONT_SIZE_LABEL_TEXT = `Font Size`
+export const MD_TOOLBAR_LABEL_TEXT = `Show Markdown Toolbar`
+
+export const SHARE_HEADER_TEXT = `Share URL`
+export const SHARE_FOOTER_TEXT = `Generated URLs can be quite long. If you like, you can use a 3rd party URL-shortening service.`
+
+
+export const PUBLISH_BTN_TEXT = `Publish`
+
+
 const guideText = `# Quick guide to using libre.ink
 
 Hello internet! 
