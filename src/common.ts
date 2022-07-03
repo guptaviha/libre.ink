7207e3c9978cbacb46942987b51a6b4f6a871072
Viha Gupta <vihagupta95@gmail.com>
2022-07-02 21:25:14 -0400

    testing helmet

--- src/common.ts
+++ src/common.ts
@@ -1,8 +1,13 @@
 import { APP_TITLE } from "./constants";
 import LZString from "lz-string";
 
-export const setTitle = (postContent: string) => {
+export const getPlainTextFirstLine = (postContent: string) => {
     const plainTextFirstLine = postContent.split('\n')[0].replace(/[#*>`]/g, '').substring(0, 150);
+    return plainTextFirstLine;
+};
+
+export const setTitle = (postContent: string) => {
+    const plainTextFirstLine = getPlainTextFirstLine(postContent);
     const title = plainTextFirstLine.trim().length ? `${plainTextFirstLine} - ${APP_TITLE}` : APP_TITLE;
     document.title = title;
 };
