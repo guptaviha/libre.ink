8b9c9937d1669d4909e3a16f28fef95f1dc83ea8
Karan Rajpal <karanrajpal001@gmail.com>
2022-09-11 19:10:45 -0400

    Landing page size concerns

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -4,8 +4,14 @@ import theme from '../theme';
 import { Link } from 'react-router-dom';
 import { FloatingControls } from './FloatingControls';
 import '../styles/main.scss';
-import tutorialGif from '../../assets/Quick-Tutorial.gif';
 // import tutorialMov from '../../assets/Quick-Tutorial.mov';
+
+function isMobile() {
+    if (navigator.userAgent.toLowerCase().match(/mobile/i)) {
+        return true;
+    }
+    return false;
+}
 
 export const Landing = () => {
     const { colorMode } = useColorMode();
@@ -19,11 +25,11 @@ export const Landing = () => {
                 flexDirection='column'
                 alignItems='center'
                 overflow='scroll'
-                paddingTop="100px"
                 paddingBottom="50px"
+                margin="60px 0"
                 className='landing-page'
             >
-                <Text fontSize='5xl' margin={'0 0 30px'}>
+                <Text fontSize='5xl' margin={'40px 0 30px'}>
                     <span style={{ color: mainTextColor }}>Instant anonymous</span> blogging
                 </Text>
                 <Text margin='0 0 25px' textAlign='center'>
@@ -35,9 +41,11 @@ export const Landing = () => {
                         Write post
                     </Button>
                 </Link>
-                <video muted loop autoPlay width="800" height="600">
-                    <source src='https://github.com/guptaviha/libre.ink/raw/main/assets/Quick-Tutorial.mov' />
-                </video>
+                <Box marginBottom={isMobile() ? "200px" : "50px"} boxShadow="0 14px 28px rgb(0 0 0 / 25%), 0 10px 10px rgb(0 0 0 / 22%);">
+                    <video muted loop autoPlay width="800" height="600">
+                        <source src='https://github.com/guptaviha/libre.ink/raw/main/assets/Quick-Tutorial.mov' />
+                    </video>
+                </Box>
                 {/* <Image
                     src={tutorialGif}
                     margin='0 0 100px'
